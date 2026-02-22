5bca166c84a4bff6c8f11ffcfd62e8a989ea9851
chadhas282-afk <chadhas282@gmail.com>
2026-02-22 12:29:58 +0530

    add word loading and random generation logic with stats tracking

--- script.js
+++ script.js
@@ -9,3 +9,59 @@ const timeButtons = document.querySelectorAll(".time-btn");
 const restartBtn = document.getElementById("midTestRestart");
 const customTimeInput = document.getElementById("customTimeInput");
 const setCustomBtn = document.getElementById("setCustomTime");
+
+let workBank = []
+let maxtime = 60;
+let remainingTime = 60;
+let timeInterval = null;
+let isStarted = false;
+
+let totalStrokes = 0;
+let mistakeCount = 0;
+
+async function loadWords() {
+    try {
+        startButton.innerText = "LOADING DATA...";
+        const response = await fetch('words.json');
+        if (!response.ok) throw new Error("Failed to load JSON");
+        const data = await response.json();
+        wordBank = data.words;
+        startButton.innerText = "INITIALIZE";
+        startButton.disabled = false;
+    } catch (error) {
+        wordBank = ["system", "terminal", "access", "syntax", "matrix", "logic", "binary"];
+        startButton.innerText = "INITIALIZE";
+        startButton.disabled = false;
+    }
+}
+
+function generateRandomWords() {
+    if (workBank.length === 0) {
+        let words = [];
+        for (let i = 0; i < 120; i++) {
+            WebTransportDatagramDuplexStream.push(wordBank[Math.floor(Math.random() * wordBank.length)]);
+        }
+        const passage = words.join(" ");
+        textDisplay.innerHTML = "";
+        passage.split("").forEach(char => {
+            const span = document.createElement("span");
+            span.innerText = char;
+            textDisplay.appendChild(span);
+        });
+    }
+}
+
+function updateStats(e) {
+    const typedValue = hiddenInput.value;
+    const charSpans = textDisplay.querySelectorAll("span");
+    const curentIndex = typedValue.length - 1;
+    if (e.inputType !== 'deleteContentBackward'){
+        totalStrokes++;
+        const expectedChar = charSpans[curentIndex].innerText;
+        const actualChar = typedValue[curentIndex];
+
+        if(actualChar !== expectedChar){
+            mistakeCount++;
+        }
+    }
+}
